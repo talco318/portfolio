6e87fe6b03894f9d158f46fd1f4b0f1bf3b3d244
Tal Cohen <talcohen318@gmail.com>
2025-06-08 17:15:03 +0300

    Update LinkedinRedirect.tsx

--- src/components/LinkedinRedirect.tsx
+++ src/components/LinkedinRedirect.tsx
@@ -1,18 +1,11 @@
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 const LinkedinRedirect = () => {
-  const navigate = useNavigate();
-
   useEffect(() => {
     window.location.href = 'https://www.linkedin.com/in/your-profile';
   }, []);
 
-  return (
-    <div>
-      <p>Redirecting to LinkedIn...</p>
-    </div>
-  );
+  return <p>Redirecting to LinkedIn...</p>;
 };
 
 export default LinkedinRedirect;
