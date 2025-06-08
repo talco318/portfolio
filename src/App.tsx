09247e5519c9dc3de79890549590051a8c9c8da7
Tal Cohen <talcohen318@gmail.com>
2025-06-08 17:16:58 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from './components/MainPage'; // Assuming MainPage.tsx exists
-import { ContactSectionNew } from './components/ContactSectionNew'; // Assuming ContactSectionNew.tsx exists
+import { LinkedinRedirect } from './components/LinkedinRedirect'; 
+
 
 function App() {
     return (
