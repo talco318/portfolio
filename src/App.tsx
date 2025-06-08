08457afb3f66d667586a3d58b43a9dca65050667
Tal Cohen <talcohen318@gmail.com>
2025-06-08 17:18:07 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from './components/MainPage'; // Assuming MainPage.tsx exists
-import { LinkedinRedirect } from './components/LinkedinRedirect'; 
+import LinkedinRedirect from './components/LinkedinRedirect';
 
 
 function App() {
